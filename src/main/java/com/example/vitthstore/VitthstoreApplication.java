5d66b4dd63e0d54e1a4101eed904ff1aa0fdb654
LuanLucasGon <lucasluangon@gmail.com>
2026-02-19 00:31:47 -0300

    first commit

---
+++ src/main/java/com/example/vitthstore/VitthstoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.vitthstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitthstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitthstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vitthstore/VitthstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vitthstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitthstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
